71b33477c23dde21a2957133704e184348eecbb9
Dario Alvarez <dario@electronic-bits.com>
2017-03-18 23:51:07 +1100

    deleting messages

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import {TodoForm, TodoList, Footer} from './components/todo'
 import {addTodo, generateId, findById, toggleTodo, updateTodo, removeTodo, filterTodos} from './lib/todohelper'
 import {pipe, partial} from './lib/utils'
-import {loadTodos, createTodo, saveTodo} from './lib/todoService'
+import {loadTodos, createTodo, saveTodo, destroyTodo} from './lib/todoService'
 
 class App extends Component {
 
@@ -25,6 +25,7 @@ class App extends Component {
     evt.preventDefault();
     const updatedTodos = removeTodo(this.state.todos, id);
     this.setState({todos: updatedTodos});
+    destroyTodo(id).then(() => this.showTempMessage('Todo Removed'))
   }
 
   handleSubmit = (evt) => {
